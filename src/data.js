50e31688a5352a5966b58b5b36675497bba05e19
Cecilia Geraldo <ceciliandrea.geraldo@gmail.com>
2019-08-28 16:46:52 -0400

    data rescatada de pokemon.js

--- src/data.js
+++ src/data.js
@@ -1,10 +1,17 @@
-/* Manejo de data */
+//Hicimos global la data en data/pokemon.js y aquí creamos la variable 
+//que almacenará los objetos pokemon y sus atributos.
+const pokemons = window.POKEMON.pokemon;
+for(let i=0; i<pokemons.length;i++){
+  //Le indicamos que cree un elemento div en nuestro HTML que contendrá toda la info
+  let cards = document.createElement("div");
+  //Aquí le diremos que tome la clase "cards" de css
+  cards.className =  "cards";
+  //Le entregaremos un id
+  cards.id = "cards";
 
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-
-const example = () => {
+}
+/*const sortByType  = () => {
   return 'example';
 };
+*/
 
-window.example = example;
