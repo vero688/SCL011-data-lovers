4b059f228822193ee920e57242450384b1374385
vero688 <vero.urrutiaa@gmila.coname>
2019-09-05 23:48:06 -0400

    cambios en el main js

--- src/data.js
+++ src/data.js
@@ -1,18 +1,5 @@
-<<<<<<< HEAD
-// filtrado de la lista de pokemon por tipo 
-const pokemons = window.POKEMON.pokemon;
-window.pokemonList=pokemonList 
-=======
  const pokemonList = window.POKEMON.pokemon;
  window.pokemonList = pokemonList;
- /*filtPo:(dataFil, condition)=>{
-        const filterPoke = dataFil.filter(element => {
-            return element.type.includes(condition) === true
-          })
-          return filterPoke;
-        } */
-
-//Filtrar por Tipo
 const filterTypes = (pokemonList, selectedType) => {
   const resultType = pokemonList.filter(Element => {
       return Element.type.includes(selectedType);
@@ -21,17 +8,18 @@ const filterTypes = (pokemonList, selectedType) => {
 }
 window.filterTypes = filterTypes;
 
-
->>>>>>> b92c1bec346f7e77d1f7600294346e6d484a9dea
-
-const filterByType=(pokemonList, selectType)=>{
-    const resultType=(pokemonList,filter(Element=>{
-        return Element.Type.includes(selectType);
-
+//filtrado por debilidad
+window.pokemonList = pokemonList;
+    const filterWeakness =(pokemonList,weaknessType) =>{
+    const weakness =pokemonList.filter(Element =>{
+        return Element.type.includes(weaknessType);
     });
-    window.filterByTypes=filterByTypes;
+    return weakness;
+    console.log("weaknessType")
 }
-    
+window.filterWeakness = filterWeakness;
+
+
      
         
     
