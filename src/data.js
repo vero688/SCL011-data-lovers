34b6fad03cb70c27692eb40a42490976013d5d1c
Cecilia Geraldo <ceciliandrea.geraldo@gmail.com>
2019-09-08 14:43:08 -0300

    añadida función filtrar por huevo con sus comentarios en el data.js

--- src/data.js
+++ src/data.js
@@ -37,6 +37,30 @@ window.filterTypes = filterTypes;
 }
 window.filterWeakness = filterWeakness;
 
+/*Queremos filtrar los pokemons que aparecen en cada huevo, mediante un selector el usuario
+indicará si quiere ver los pókemons en huevos de 2,5,7,10,15, o que no aparecen en huevos.
+Trabajaremos nuevamente con la lista completa de pokemons y el criterio que vendría siendo
+la selección del usuario.*/
+const filterEggs = (pokemonList, eggSelected)=>{
+  /*En una variable almacenaremos el array que se formará al llamar a la función filter
+  donde ya tendremos todos los resultados que coinciden con el criterio de búsqueda*/
+  const filteredEggs = pokemonList.filter(Element =>{
+    /*El criterio de búsqueda en este caso, es que en pokemon.js : pokemon.egg = a la 
+    selección del usuario */
+    return Element.egg ===eggSelected;
+  })
+  /*Retornamos nuestra variable donde almacenamos previamente lo que resultaba de la función
+  filtrar*/
+  return filteredEggs;
+}
+//Nuevamente hacemos de nuestra función global para poder llamarla desde main.js
+window.filterEggs = filterEggs;
+/*
+const sortAlphabetical = (pokemonList)=>{
+  return pokemonList.sort();
+}
+
+window.sortAlphabetical = sortAlphabetical;*/
 
 
     
--- src/main.js
+++ src/main.js
@@ -109,7 +109,7 @@ function showByType()
   
   //Le indicamos que cree un elemento div en nuestro HTML que contendrá toda la info
   let cards = document.createElement("div");
-  //Aquí le diremos que tome la clase "cards" de css
+  //Aquí le diremos que tome la clase "cards" de css <div id="cards" class="cards"></div>
   cards.className =  "cards";
   //Le entregaremos un id
   cards.id = "cards";
@@ -240,6 +240,190 @@ function showByType()
       columnType2.appendChild(pokemonType2);
       columnType3.appendChild(pokemonType3);
       columnType4.appendChild(pokemonType4);
+      rowType.appendChild(columnType1);
+      rowType.appendChild(columnType2);
+      rowType.appendChild(columnType3);
+      rowType.appendChild(columnType4);
+      cards.appendChild(pokemonName);
+      cards.appendChild(pokemonPhoto);
+      cards.appendChild(pokemonNumber);
+      cards.appendChild(rowType);
+  
+      columnCard.appendChild(cards);
+  
+      //Mostrar tarjeta en el contenedor especificado
+      document.getElementById("root").appendChild(columnCard).innerHTML; 
+  
+  }
+    }
+
+
+const selectorEgg = document.getElementById("filterEggs");
+selectorEgg.addEventListener("change",showByEgg);
+
+function showByEgg()
+{
+  let eggSelected = selectorEgg.options[selectorEgg.selectedIndex].value;
+  let filterResult = window.filterEggs(pokemons, eggSelected);
+  document.getElementById("root").innerHTML="";
+
+  for(let i=0; i<filterResult.length; i++){
+    console.log(filterResult);  
+    
+  //Div de clase columna, cada tarjeta corresponderá a una columna dentro de la fila
+  let columnCard = document.createElement("div");
+  columnCard.className = "column";
+  //Crearemos una etiqueta <a> que envolverá la tarjeta, para poder llamar la apertura
+  //de un modal con href
+  
+  let linkModal = document.createElement("a");
+  linkModal.href = 'nada'; 
+
+  
+  //Le indicamos que cree un elemento div en nuestro HTML que contendrá toda la info
+  let cards = document.createElement("div");
+  //Aquí le diremos que tome la clase "cards" de css
+  cards.className =  "cards";
+  //Le entregaremos un id
+  cards.id = "cards";
+
+  let pokemonName = document.createElement("h4");
+  //Para rellenar el elemento h4, al ser una etiqueta de texto utilizamos textContent 
+  //para asignarle su contenido
+  //En pokemon.js "name"=""
+  pokemonName.textContent = filterResult[i].name;
+  
+  //Creamos el elemento img que conteneŕa la imagen de cada pokemon
+  let pokemonPhoto = document.createElement("img");
+  //Le decimos que en este elemento HTML su source(src="") será lo que contiene
+  //el elemento img:'' dentro de la data, según sea el índice.
+  //En pokemon.js "img"=""
+
+  pokemonPhoto.src = filterResult[i].img;
+
+  let pokemonNumber = document.createElement("h3");
+  //En pokemon.js "num"=""  
+  pokemonNumber.textContent = filterResult[i].num;
+
+  let rowType = document.createElement("div");
+  rowType.className = "row";
+  rowType.id = "rowType";
+  let columnType1 = document.createElement("div");
+  columnType1.className = "column";
+  let columnType2 = document.createElement("div");
+  columnType2.className = "column";
+  let columnType3 = document.createElement("div");
+  columnType3.className = "column";
+  let columnType4 = document.createElement("div");
+  columnType4.className = "column";
+  //Tiene uno o dos tipos
+  //Ideal sería hacer un if, que si tiene un tipo entonces cree un label y de acuerdo
+  //sea el tipo, será la clase que tomará (para que sean de colores distintos) y si son
+  //dos tipos entones que cree dos label.
+  let pokemonType1 = document.createElement("h1");
+  pokemonType1.textContent = filterResult[i].type[0];
+  let pokemonType2 = document.createElement("h1");
+  pokemonType2.textContent = filterResult[i].type[1];
+  
+  /*let pokemonType3 = document.createElement("h1");
+  pokemonType1.textContent = filterResult[i].weaknesses[0];
+  let pokemonType4 = document.createElement("h1");
+  pokemonType2.textContent = filterResult[i].weaknesses[1];*/
+
+    columnType1.appendChild(pokemonType1);
+    columnType2.appendChild(pokemonType2);
+    //columnType3.appendChild(pokemonType3);
+    //columnType4.appendChild(pokemonType4);
+    rowType.appendChild(columnType1);
+    rowType.appendChild(columnType2);
+    rowType.appendChild(columnType3);
+    rowType.appendChild(columnType4);
+    cards.appendChild(pokemonName);
+    cards.appendChild(pokemonPhoto);
+    cards.appendChild(pokemonNumber);
+    cards.appendChild(rowType);
+
+    columnCard.appendChild(cards);
+
+    //Mostrar tarjeta en el contenedor especificado
+    document.getElementById("root").appendChild(columnCard).innerHTML; 
+
+}
+  }
+
+  /*
+  function sortByAlphabet()
+  {
+    let sortedPokemons = window.sortByAlphabet(pokemonList);
+    document.getElementById("root").innerHTML="";
+  
+    for(let i=0; i<sortedPokemons.length; i++){
+      console.log(sortedPokemons);  
+      
+    //Div de clase columna, cada tarjeta corresponderá a una columna dentro de la fila
+    let columnCard = document.createElement("div");
+    columnCard.className = "column";
+    //Crearemos una etiqueta <a> que envolverá la tarjeta, para poder llamar la apertura
+    //de un modal con href
+    
+    let linkModal = document.createElement("a");
+    linkModal.href = 'nada'; 
+  
+    
+    //Le indicamos que cree un elemento div en nuestro HTML que contendrá toda la info
+    let cards = document.createElement("div");
+    //Aquí le diremos que tome la clase "cards" de css
+    cards.className =  "cards";
+    //Le entregaremos un id
+    cards.id = "cards";
+  
+    let pokemonName = document.createElement("h4");
+    //Para rellenar el elemento h4, al ser una etiqueta de texto utilizamos textContent 
+    //para asignarle su contenido
+    //En pokemon.js "name"=""
+    pokemonName.textContent = sortedPokemons[i].name;
+    
+    //Creamos el elemento img que conteneŕa la imagen de cada pokemon
+    let pokemonPhoto = document.createElement("img");
+    //Le decimos que en este elemento HTML su source(src="") será lo que contiene
+    //el elemento img:'' dentro de la data, según sea el índice.
+    //En pokemon.js "img"=""
+  
+    pokemonPhoto.src = sortedPokemons[i].img;
+  
+    let pokemonNumber = document.createElement("h3");
+    //En pokemon.js "num"=""  
+    pokemonNumber.textContent = sortedPokemons[i].num;
+  
+    let rowType = document.createElement("div");
+    rowType.className = "row";
+    rowType.id = "rowType";
+    let columnType1 = document.createElement("div");
+    columnType1.className = "column";
+    let columnType2 = document.createElement("div");
+    columnType2.className = "column";
+    let columnType3 = document.createElement("div");
+    columnType3.className = "column";
+    let columnType4 = document.createElement("div");
+    columnType4.className = "column";
+    //Tiene uno o dos tipos
+    //Ideal sería hacer un if, que si tiene un tipo entonces cree un label y de acuerdo
+    //sea el tipo, será la clase que tomará (para que sean de colores distintos) y si son
+    //dos tipos entones que cree dos label.
+    let pokemonType1 = document.createElement("h1");
+    pokemonType1.textContent = sortedPokemons[i].type[0];
+    let pokemonType2 = document.createElement("h1");
+    pokemonType2.textContent = sortedPokemons[i].type[1];
+    
+    //let pokemonType3 = document.createElement("h1");
+    //pokemonType1.textContent = filterResult[i].weaknesses[0];
+    //let pokemonType4 = document.createElement("h1");
+    //pokemonType2.textContent = filterResult[i].weaknesses[1];
+  
+      columnType1.appendChild(pokemonType1);
+      columnType2.appendChild(pokemonType2);
+      //columnType3.appendChild(pokemonType3);
+      //columnType4.appendChild(pokemonType4);
       rowType.appendChild(columnType1);
       rowType.appendChild(columnType2);
       rowType.appendChild(columnType3);
